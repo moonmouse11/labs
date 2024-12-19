25a37fc2b354e983dfca7c6895f40d6b98743fea
moonmouse11 <mmssikg@gmail.com>
2024-12-19 22:30:14 +0500

    Final tables HTML

--- lab5/public/index.php
+++ lab5/public/index.php
@@ -15,12 +15,12 @@ echo '</pre>';
             <caption>Список залогов</caption>
             <thead>
             <tr>
-                <th>Title</th>
-                <th>Price</th>
-                <th>Client</th>
-                <th>Expert</th>
-                <th>Date Start</th>
-                <th>Date Over</th>
+                <th>Название</th>
+                <th>Цена</th>
+                <th>Клиент</th>
+                <th>Оценщик</th>
+                <th>Дата начала</th>
+                <th>Дата окончания</th>
             </tr>
             </thead>
             <tbody>
@@ -28,18 +28,96 @@ echo '</pre>';
             foreach ($pledgesList as $pledge) : ?>
                 <tr>
                     <td><?= $pledge['name'] ?></td>
-                    <td><?= $pledge['name'] ?></td>
-                    <td><?= $pledge['name'] ?></td>
-                    <td><?= $pledge['name'] ?></td>
+                    <td><?= $pledge['price'] ?></td>
+                    <td><?= $pledge['full_name'] ?></td>
+                    <td><?= $pledge['expert_full_name'] ?></td>
+                    <td><?= $pledge['start_date'] ?></td>
+                    <td><?= $pledge['over_date'] ?></td>
+                    <td style="border: none; text-align: left">
+                        <button class="update">Редактировать</button>
+                        <button class="delete">Удалить</button>
+                    </td>
                 </tr>
             <?php
             endforeach; ?>
+            <td style="border: none; text-align: left">
+                <button class="create">Добавить</button>
+            </td>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="table_component" role="region" tabindex="0">
+        <table>
+            <caption>Список клиентов</caption>
+            <thead>
+            <tr>
+                <th>Имя</th>
+                <th>Номер телефона</th>
+                <th>Номер паспорта</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach (
+                $clientsList
+
+                as $client
+            ) : ?>
+                <tr>
+                    <td><?= $client['full_name'] ?></td>
+                    <td><?= $client['phone'] ?></td>
+                    <td><?= $client['passport_number'] ?></td>
+                    <td style="border: none; text-align: left">
+                        <button class="update">Редактировать</button>
+                        <button class="delete">Удалить</button>
+                    </td>
+                </tr>
+            <?php
+            endforeach; ?>
+            <td style="border: none; text-align: left">
+                <button class="create">Добавить</button>
+            </td>
+            </tbody>
+        </table>
+    </div>
+    <div class="table_component" role="region" tabindex="0">
+        <table>
+            <caption>Список оценщиков</caption>
+            <thead>
+            <tr>
+                <th>Имя</th>
+                <th>Номер телефона</th>
+                <th>Дата приема на работу</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach (
+                $expertsList
+
+                as $expert
+            ) : ?>
+                <tr>
+                    <td><?= $expert['full_name'] ?></td>
+                    <td><?= $expert['phone'] ?></td>
+                    <td><?= $expert['hiring_date'] ?></td>
+                    <td style="border: none; text-align: left">
+                        <button class="update">Редактировать</button>
+                        <button class="delete">Удалить</button>
+                    </td>
+                </tr>
+            <?php
+            endforeach; ?>
+            <td style="border: none; text-align: left">
+                <button class="create">Добавить</button>
+            </td>
             </tbody>
         </table>
     </div>
     <pre>
 <?php
-                var_dump($pledgesList); ?>
+//                var_dump($pledgesList); ?>
     </pre>
 <?php
 require 'templates/footer.php';
--- lab5/public/templates/header.php
+++ lab5/public/templates/header.php
@@ -10,21 +10,23 @@ require '../vendor/autoload.php';
         .table_component {
             overflow: auto;
             width: 100%;
+            place-items: center;
+            margin-bottom: 50px;
         }
 
         .table_component table {
-            border: 1px solid #dededf;
-            height: 100%;
-            width: 100%;
-            table-layout: fixed;
+            height: max-content;
+            width: 900px;
+            table-layout: auto;
             border-collapse: collapse;
-            border-spacing: 1px;
-            text-align: left;
+            border: 1px none #dededf;
+            text-align: center;
         }
 
         .table_component caption {
             caption-side: top;
-            text-align: left;
+            font-size: x-large;
+            margin-bottom: 20px;
         }
 
         .table_component th {
@@ -40,7 +42,51 @@ require '../vendor/autoload.php';
             color: #000000;
             padding: 5px;
         }
+
+        .table_component button.delete {
+            text-align: left;
+            text-decoration: none;
+            display: inline-block;
+            border: none;
+            background-color: red;
+            color: black;
+            padding: 5px;
+            cursor: pointer;
+        }
+
+        .table_component button.update {
+            text-align: left;
+            text-decoration: none;
+            display: inline-block;
+            border: none;
+            background-color: dodgerblue;
+            color: black;
+            padding: 5px;
+            cursor: pointer;
+        }
+
+        .table_component button.create {
+            place-items: normal;
+            position: initial;
+            text-decoration: none;
+            display: inline-block;
+            border: none;
+            background-color: green;
+            color: black;
+            padding: 5px;
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
+<nav>
+    <ul>
+        <li>
+            <a href="index.php">Главная страница</a>
+        </li>
+        <li>
+            <a href="report.php">Отчеты</a>
+        </li>
+    </ul>
+</nav>
 
