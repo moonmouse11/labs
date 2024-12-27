4164d041e203dfb02b0c49593f202359b2510200
moonmouse11 <mmssikg@gmail.com>
2024-12-27 16:56:57 +0500

    Complete Front Task4

---
+++ lab6/task4/functions/functions.php
@@ -0,0 +1,79 @@
+<?php
+
+const MAIN_XML_FILE = __DIR__ . '/../resources/students.xml';
+const TEMP_UPLOAD_FOLDER = __DIR__ . '/../resources/uploads/';
+
+function handleRequest($request)
+{
+    var_dump($request);
+}
+
+function checkXMLFile()
+{
+    $domDocument = new DOMDocument('1.0', 'UTF-8');
+
+    if (!empty($_FILES['fileToUpload']['tmp_name'])) {
+        /* Подгрузка пользовательского XML */
+        copy($_FILES['fileToUpload']['tmp_name'], TEMP_UPLOAD_FOLDER . $_FILES['fileToUpload']['name']);
+        $domDocument->load($_FILES['fileToUpload']['tmp_name']);
+    } elseif (checkTempFiles()) {
+        /* Подгрузка ранее загруженного пользовательского XML */
+        $domDocument->load(getLatestUploadedFile());
+    } else {
+        /* Подгрузка исходного XML */
+        copy(MAIN_XML_FILE, TEMP_UPLOAD_FOLDER . 'default.xml');
+        $domDocument->load(MAIN_XML_FILE);
+    }
+
+    return $domDocument;
+}
+
+function checkTempFiles()
+{
+    $files = scandir(TEMP_UPLOAD_FOLDER, SCANDIR_SORT_DESCENDING);
+
+    foreach ($files as $file) {
+        if ($file === '.' || $file === '..' || $file === '.gitignore') {
+            continue;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+function getLatestUploadedFile()
+{
+    $files = scandir(TEMP_UPLOAD_FOLDER, SCANDIR_SORT_DESCENDING);
+
+    $tempFile = '';
+    $fileTimeMax = 0;
+
+    foreach ($files as $file) {
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+        if ($fileTimeMax < filemtime(TEMP_UPLOAD_FOLDER . $file)) {
+            $tempFile = TEMP_UPLOAD_FOLDER . $file;
+            $fileTimeMax = filemtime(TEMP_UPLOAD_FOLDER . $file);
+        }
+    }
+
+    return $tempFile;
+}
+
+function createRecord($record)
+{
+
+
+}
+
+function updateRecord($recordId, $record)
+{
+
+}
+
+function deleteRecord($recordId)
+{
+
+}
