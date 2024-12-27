52a92d70f5bf874641b1ba9670f1170182cf0769
moonmouse11 <mmssikg@gmail.com>
2024-12-27 18:52:47 +0500

    Complete Lab 6

--- lab6/task4/functions/functions.php
+++ lab6/task4/functions/functions.php
@@ -5,7 +5,24 @@ const TEMP_UPLOAD_FOLDER = __DIR__ . '/../resources/uploads/';
 
 function handleRequest($request)
 {
-    var_dump($request);
+    if (array_key_exists('delete_attribute', $request)) {
+        deleteAttribute($request['attribute_name'], $request['delete_attribute']);
+    }
+    if (array_key_exists('update_attribute', $request)) {
+        updateAttribute($request, $request['attribute_name_old'], $request['update_attribute']);
+    }
+    if (array_key_exists('create_attribute', $request)) {
+        createAttribute($request, $request['record_id']);
+    }
+    if (array_key_exists('delete_element', $request)) {
+        deleteElement($request['element_name'], $request['delete_element']);
+    }
+    if (array_key_exists('update_element', $request)) {
+        updateElement($request['element_name_old'], $request, $request['update_element']);
+    }
+    if (array_key_exists('create_element', $request)) {
+        createElement($request, $request['record_id']);
+    }
 }
 
 function checkXMLFile()
@@ -62,18 +79,101 @@ function getLatestUploadedFile()
     return $tempFile;
 }
 
-function createRecord($record)
+function createElement($element, $recordId)
 {
+    $domDocument = checkXMLFile();
+
+    $studentNode = $domDocument->getElementsByTagName('student')->item($recordId);
 
 
+    $newNode = $domDocument->createElement($element['element_name']);
+    $newTextNode = $domDocument->createTextNode($element['element_value']);
+
+    $newNode->appendChild($newTextNode);
+    $studentNode->appendChild($newNode);
+
+    $domDocument->save(getLatestUploadedFile());
 }
 
-function updateRecord($recordId, $record)
+function updateElement($oldElement, $newElement, $recordId)
 {
+    $domDocument = checkXMLFile();
 
+    $studentNode = $domDocument->getElementsByTagName('student')->item($recordId);
+
+    $studentNode->removeChild($studentNode->getElementsByTagName($oldElement)->item(0));
+
+    $newNode = $domDocument->createElement($newElement['element_name']);
+    $newTextNode = $domDocument->createTextNode($newElement['element_value']);
+
+    $newNode->appendChild($newTextNode);
+    $studentNode->appendChild($newNode);
+
+    $domDocument->save(getLatestUploadedFile());
 }
 
-function deleteRecord($recordId)
+function deleteElement($elementName, $recordId)
 {
+    $domDocument = checkXMLFile();
 
+    $deletedElement = $domDocument->getElementsByTagName('student')->item($recordId);
+
+    $deletedElement->removeChild($deletedElement->getElementsByTagName($elementName)->item(0));
+
+    $domDocument->save(getLatestUploadedFile());
+}
+
+function createAttribute($attribute, $recordId)
+{
+    $domDocument = checkXMLFile();
+
+    $product = $domDocument->getElementsByTagName('student')->item($recordId);
+
+    $product->setAttribute($attribute['attribute_name'], $attribute['attribute_value']);
+
+    $domDocument->save(getLatestUploadedFile());
+}
+
+function updateAttribute($newAttribute, $oldAttribute, $recordId)
+{
+    $domDocument = checkXMLFile();
+
+    $product = $domDocument->getElementsByTagName('student')->item($recordId);
+
+    $product->removeAttribute($oldAttribute);
+
+    $product->setAttribute($newAttribute['attribute_name'], $newAttribute['attribute_value']);
+
+    $domDocument->save(getLatestUploadedFile());
+}
+
+function deleteAttribute($attributeTitle, $recordId)
+{
+    $domDocument = checkXMLFile();
+
+    $product = $domDocument->getElementsByTagName('student')->item($recordId);
+
+    $product->removeAttribute($attributeTitle);
+
+    $domDocument->save(getLatestUploadedFile());
+}
+
+function serachElement($search)
+{
+    $result = [];
+
+    $domDocument = checkXMLFile();
+
+    $count = $domDocument->getElementsByTagName('student')->count();
+
+    for ($item = 0; $item < $count; $item++) {
+        $students = $domDocument->getElementsByTagName('student')->item($item)->childNodes;
+        foreach ($students as $student) {
+            if (str_contains(mb_strtoupper($student->nodeValue), mb_strtoupper($search))) {
+                $result[$item] = $domDocument->getElementsByTagName('student')->item($item);
+            }
+        }
+    }
+
+    return $result;
 }
--- lab6/task4/index.php
+++ lab6/task4/index.php
@@ -4,6 +4,8 @@ require 'functions/functions.php';
 
 handleRequest($_POST);
 
+$searchResult = serachElement($_POST['search']);
+
 $domDocument = checkXMLFile();
 ?>
 
@@ -14,12 +16,90 @@ $domDocument = checkXMLFile();
 </head>
 <body>
 <div style="margin-bottom: 40px">
-    <form action="/" method="post" enctype="multipart/form-data">
+    <form action="/" method="POST" enctype="multipart/form-data">
         Select XML to upload:
         <input type="file" name="fileToUpload" id="fileToUpload">
         <input type="submit" value="Upload XML" name="submit">
     </form>
 </div>
+<div style="margin-bottom: 40px">
+    <form action="/" method="POST">
+        Search:
+        <input type="text" name="search" id="search" placeholder="text here">
+        <input type="submit" value="Search" name="submit">
+    </form>
+</div>
+<?php if(!empty($searchResult)): ?>
+        <h4>Результаты поиска</h4>
+        <?php foreach ($searchResult as $item => $student) : ?>
+        <table>
+            <caption>Атрибуты</caption>
+            <?php
+            foreach ($student->attributes as $attribute) :
+                $title = $attribute->nodeName;
+                $name = $attribute->nodeValue; ?>
+                <tr>
+                    <form action="/" method="POST" id="update_attribute_<?= $item . '_' . $title ?>">
+                        <td><input type="text" id="<?= $title ?>" required name="attribute_name" value="<?= $title ?>"></td>
+                        <td><input type="text" id="<?= $name ?>" required name="attribute_value" value="<?= $name ?>"></td>
+                        <input type="hidden" name="attribute_name_old" value="<?= $title ?>">
+                    </form>
+                    <form action="/" method="POST" id="delete_attribute_<?= $item . '_' . $title ?>">
+                        <input type="hidden" name="attribute_name" value="<?= $title ?>"></form>
+                    <td style="border: none; text-align: left">
+                        <button form="update_attribute_<?= $item . '_' . $title ?>" name="update_attribute" value="<?= $item ?>">Изменить</button>
+                        <button form="delete_attribute_<?= $item . '_' . $title ?>" name="delete_attribute" value="<?= $item ?>">Удалить</button>
+                    </td>
+                </tr>
+            <?php
+            endforeach; ?>
+            <tr>
+                <form action="/" method="POST" id="create_attribute_<?= $item ?>">
+                    <td><input type="text" id="attribute_name" required name="attribute_name" value=""></td>
+                    <td><input type="text" id="attribute_value" required name="attribute_value" value=""></td>
+                    <input type="hidden" name="record_id" value="<?= $item ?>">
+                    <td><input type="submit" name="create_attribute" value="Добавить"></td>
+                </form>
+            </tr>
+        </table>
+        <table>
+            <caption>Элементы</caption>
+            <?php
+            foreach ($student->childNodes as $name) :
+                if ($name->nodeName !== '#text') :
+                    $title = $name->nodeName;
+                    $name = $name->nodeValue;
+                    ?>
+                    <tr>
+                        <form action="/" method="POST" id="update_element_<?= $item . '_' . $title ?>">
+                            <td><input type="text" id="<?= $title ?>" required name="element_name" value="<?= $title ?>"></td>
+                            <td><input type="text" id="<?= $name ?>" required name="element_value" value="<?= $name ?>"></td>
+                            <input type="hidden" name="element_name_old" value="<?= $title ?>">
+                        </form>
+                        <form action="/" method="POST" id="delete_element_<?= $item . '_' . $title ?>">
+                            <input type="hidden" name="element_name" value="<?= $title ?>"></form>
+                        <td style="border: none; text-align: left">
+                            <button form="update_element_<?= $item . '_' . $title ?>" name="update_element" value="<?= $item ?>">Изменить</button>
+                            <button form="delete_element_<?= $item . '_' . $title ?>" name="delete_element" value="<?= $item ?>">Удалить</button>
+                        </td>
+                    </tr>
+                <?php
+                endif; ?>
+            <?php
+            endforeach; ?>
+            <tr>
+                <form action="/" method="POST" id="create_element_<?= $item ?>">
+                    <td><input type="text" id="element_name" required name="element_name" value=""></td>
+                    <td><input type="text" id="element_value" required name="element_value" value=""></td>
+                    <input type="hidden" name="record_id" value="<?= $item ?>">
+                    <td><input type="submit" name="create_element" value="Добавить"></td>
+                </form>
+            </tr>
+        </table>
+<?php endforeach; ?>
+<?php else:; ?>
+<h3>Записи не найдены</h3>
+<?php endif; ?>
 <div>
     <h2>Список студентов</h2>
 </div>
